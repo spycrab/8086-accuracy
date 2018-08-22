24014eeff6f095ed716e5969c05d26c52c09e0e1
spycrab <spycrab@users.noreply.github.com>
2018-08-23 01:41:24 +0200

    cmp: Add additional subtest

--- src/cmp.s
+++ src/cmp.s
@@ -1,34 +1,48 @@
-test_cmp:       
-        printc '1'
+test_cmp: printc '1'
 
-        mov ah, 0xff        
-        
-        cmp ah, ah
-        jnz .fail
-        jne .fail
-        jg  .fail
-        ja  .fail
-        jnp .fail
-        jl  .fail
-        jo  .fail
-        js  .fail
+          mov ah, 0xff
 
-        printc '2'
+          cmp ah, ah
+          jnz .fail
+          jne .fail
+          jg  .fail
+          ja  .fail
+          jnp .fail
+          jl  .fail
+          jo  .fail
+          js  .fail
 
-        mov ax, 0x1000
-        mov bx, 0x2001
+          printc '2'
 
-        cmp ax, bx
-        jz  .fail
-        je  .fail
-        jg  .fail
-        ja  .fail
-        jnl .fail
-        jns .fail
-        jo  .fail
+          mov ax, 0x1000
+          mov bx, 0x2001
 
-        mov ax, 1
-        ret
+          cmp ax, bx
+          jz  .fail
+          je  .fail
+          jg  .fail
+          ja  .fail
+          jnl .fail
+          jns .fail
+          jo  .fail
+
+          printc '3'
+
+          mov ah, 0
+          mov bl, 0xff
+
+          cmp ah, bl
+          jz  .fail
+          jnc .fail
+          je  .fail
+          jng .fail
+          ja  .fail
+          jl  .fail
+          js  .fail
+          jo  .fail
+
+          mov ax, 1
+          ret
 
 .fail:  mov ax, 0
         ret
