d7f70c37a723e1263c72c320c6cef684fba0afec
spycrab <spycrab@users.noreply.github.com>
2018-09-16 15:19:36 +0200

    add: Extend and improve tests

--- src/add.s
+++ src/add.s
@@ -1,15 +1,43 @@
 test_add: printc '1'
+
           mov al, 0xff
+          add al, 0x01
 
-          add al, 0x07
+          JUMP_FAIL jo
+          JUMP_FAIL jl
+          JUMP_FAIL jg
+          JUMP_FAIL jnp
+          JUMP_FAIL jnc
+          JUMP_FAIL ja
+          JUMP_FAIL jnz
+          JUMP_FAIL js
 
+          printc '2'
+
+          mov ax, 0x10
+          add ax, 0x40
+
+          JUMP_FAIL jo
           JUMP_FAIL jl
           JUMP_FAIL jng
+          JUMP_FAIL jnp
+          JUMP_FAIL jc
+          JUMP_FAIL jna
+          JUMP_FAIL jz
+          JUMP_FAIL js
+
+          printc '3'
+
+          mov bx, 0xfff0
+          add bx, 0xf000
+
+          JUMP_FAIL jo
+          JUMP_FAIL jg
           JUMP_FAIL jnp
           JUMP_FAIL jnc
           JUMP_FAIL ja
           JUMP_FAIL jz
-          JUMP_FAIL js
+          JUMP_FAIL jns
 
           mov ax, 1
           ret
